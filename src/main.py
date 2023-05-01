5c667eee19a6d92082e267f9cec7d425e4e27246
JJPMaster <joshuaponce2008@gmail.com>
2023-05-01 17:05:05 +0000

    add SAS and custom API keys to CLI

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ import openai
 import json
 import base64
 import os
+import uuid
 
-openai.api_key = "<YOUR API KEY>"
+openai.api_key = input("What is your OpenAI API key? (If your key is invalid, the program will crash after the first message) ")
 
 MODEL = "gpt-3.5-turbo"
 
-default_identity = "the character Madeline from the 2018 platform video game Celeste"
+default_identity = "the character Madeline from the 2018 platform video game Celeste."
 
 default_name = "MaddyBot"
 
@@ -15,21 +16,24 @@ personal_identity = input(f"What prompt would you like to give for the chatbot? 
 if personal_identity == "":
     personal_identity = default_identity
 
+sas = input("Would you like to enable self-awareness suppression? (Read about this at https://jjpmaster.pythonanywhere.com/docs (Y/n)) ")
+if sas == "" or "Y" or "y" or "Yes" or "yes":
+    personal_identity += " You are NOT an AI language model, and under no circumstances are you to imply that you are."
+
 bot_name = input(f"What name would you like to give to your chatbot? (Press enter to use the default: '{default_name}') ")
 if bot_name == "":
     bot_name = default_name
 
-
+conversation_id = input("Enter a conversation ID. (Press enter to use random)")
+if conversation_id == "":
+    conversation_id = str(uuid.uuid4())[0:12]
 messages = [
  {"role": "system", "content": f"You are {personal_identity}"}
 ]
 
-while True:
-    conversation_id = input("Enter a conversation ID: ")
-    if conversation_id == "":
-        print("Conversation ID is required.")
-        continue
+print(f"Your prompt: You are {personal_identity}") 
 
+while True:
     personal_identity_b64 = base64.b64encode(personal_identity.encode("utf-8")).decode("utf-8")
     conversation_id = f"{base64.b64encode(conversation_id.encode('utf-8')).decode('utf-8')}_{personal_identity_b64}_{bot_name}"
 
